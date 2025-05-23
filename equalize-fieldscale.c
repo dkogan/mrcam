230982b7f8fc94ea347211de13af1004ef249f33
Dima Kogan <dima@secretsauce.net>
2025-05-23 12:08:18 -0700

    comment

--- equalize-fieldscale.c
+++ equalize-fieldscale.c
@@ -1,3 +1,23 @@
+/*
+An equalization routine meant to boost the local contrast in 16-bit images.
+Originally based on:
+
+  Fieldscale: Locality-Aware Field-based Adaptive Rescaling for Thermal Infrared
+  Image Hyeonjae Gil, Myeon-Hwan Jeon, and Ayoung Kim
+
+  @article{gil2024fieldscale,
+    title={Fieldscale: Locality-Aware Field-based Adaptive Rescaling for Thermal Infrared Image},
+    author={Gil, Hyeonjae and Jeon, Myung-Hwan and Kim, Ayoung},
+    journal={IEEE Robotics and Automation Letters},
+    year={2024},
+    publisher={IEEE}
+  }
+
+  Original author: Hyeonjae Gil
+  Author email: h.gil@snu.ac.kr
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
