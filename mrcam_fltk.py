d4b084c55e91a28ac1f86d8e958a4e517768605d
Dima Kogan <dima@secretsauce.net>
2025-08-02 11:20:53 -0700

    cb_status_value has the correct default

--- mrcam_fltk.py
+++ mrcam_fltk.py
@@ -1414,7 +1414,7 @@ def fltk_application_init(camera_params_noname,
 
                           cb_create_gui_elements = create_gui_elements__default,
                           cb_displayed_image     = displayed_image__default,
-                          cb_status_value        = None,
+                          cb_status_value        = status_value__default,
                           # other stuff from the contexts that I don't need here
                           **kwargs
                           ):
